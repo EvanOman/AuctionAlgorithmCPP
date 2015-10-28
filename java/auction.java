ef0839a6059d2062ce83d3b9842513ba30efff1a
Evan Oman <evan058@gmail.com>
2015-10-28 14:53:06 -0500

    Reorganizing

---
+++ java/auction.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class auction
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        a.add(2);
+        System.out.println(a);
+        makeRandC(a);
+        System.out.println(a);
+        System.out.println("Give me an int pls");
+        int myInt = keyboard.nextInt();    
+        System.out.println("You entered a " + myInt);
+    }
+
+    public static void auction(int N)
+    {
+
+    }
+
+    public static void auctionRound(Matrix assignment, List<Double> prices, 
+
+    public static void  makeRandC(ArrayList<Integer> a)
+    {
+        a.add(3);
+    }
+}
